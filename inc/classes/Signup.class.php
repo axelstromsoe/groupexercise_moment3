bd72c6634624ef717b8ddd4c2710a6f12b080a57
axelstromsoe <axel.stromsoe@gmail.com>
2023-02-08 10:06:39 +0100

    Förändirngar i Signup-klassen

--- inc/classes/Signup.class.php
+++ inc/classes/Signup.class.php
@@ -3,9 +3,40 @@
 class Signup {
 
     /* ------ Egenskaper ------ */
-    
+    private $email;
+    private $email_list;
     
     /* ------ Metoder ------ */
+
+    // Konstruerare
+    public function __constructor(string $email) {
+
+        $this->email = $email;
+    }
+
+    // set-metod
+    public function setEmail(string $email) : bool {
+
+        if (strlen($email) > 0) {
+
+            $this->email = $email;
+            return true;
+        } else {
+
+            return false;
+        }
+    }
+
+    // Sparar e-post
+    public function saveEmail() : bool {
+        return true;
+    }
+
+    // get-metoder
+    public function getList() : array {
+
+        return $this->email_list;
+    }
 }
 
 ?>
