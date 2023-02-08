5c904c15794f5fe5490623193f35decdd09257b0
axelstromsoe <axel.stromsoe@gmail.com>
2023-02-08 10:21:55 +0100

    Förändringar i kontrueraren

--- inc/classes/Signup.class.php
+++ inc/classes/Signup.class.php
@@ -1,21 +1,39 @@
 <?php
 
-class Signup {
+class Signup
+{
 
     /* ------ Egenskaper ------ */
     private $email;
     private $email_list;
-    
+
     /* ------ Metoder ------ */
 
     // Konstruerare
-    public function __constructor(string $email) {
+    public function __constructor(string $email)
+    {
 
         $this->email = $email;
+
+        // Kontroll ifall human.json finns
+        if (file_exists("email.json")) {
+
+            // Läsa in och konvererar JSON-filen
+            $jsonFile = file_get_contents("email.json");
+            $email_list = json_decode($jsonFile, true);
+
+            // Sparar till email_list
+            $this->email_list = $email_list;
+
+        } else {
+
+            $this->email_list = [];
+        }
     }
 
     // set-metod
-    public function setEmail(string $email) : bool {
+    public function setEmail(string $email): bool
+    {
 
         if (strlen($email) > 0) {
 
@@ -28,15 +46,15 @@ class Signup {
     }
 
     // Sparar e-post
-    public function saveEmail() : bool {
+    public function saveEmail(): bool{
+
         return true;
     }
 
     // get-metoder
-    public function getList() : array {
+    public function getList(): array
+    {
 
         return $this->email_list;
     }
 }
-
-?>
