80fa43326f3ccab934257fff43dccdeaa4284051
Anders Sundin <olsu2201@student.miun.se>
2023-02-08 10:02:23 +0100

    lägger till namn

--- index.php
+++ index.php
@@ -7,6 +7,10 @@ use function PHPSTORM_META\type;
   <h1>Anmäl ditt intresse att delta i föreläsning</h1>
 
  <form action="index.php" method="POST">
+  <label for="name">Ditt namn:</label>
+  <input type="text" name="name" id="name">
+  <br>
+  <br>
   <label for="email">Lägg till emailadress:</label>
   <input type="email" name="email" id="email" autocomplete="off">
   <br>
